b1ade15a40ad5ae62ec906cd46731344a605b36c
gdmec07150809 <672423400@qq.com>
2017-11-13 18:47:31 +0800

    提交遗漏文件，按时间段查询的文件

---
+++ app/src/main/java/com/example/administrator/ding_small/SearchTimeActivity.java
@@ -0,0 +1,40 @@
+package com.example.administrator.ding_small;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2017/11/13.
+ */
+
+public class SearchTimeActivity extends Activity {
+    private TextView start_date,end_date;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_time);
+        start_date= (TextView) findViewById(R.id.start_time);
+        end_date= (TextView) findViewById(R.id.end_time);
+        Button btn= (Button) findViewById(R.id.confirm);
+        btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DatePicker datePicker = (DatePicker) findViewById(R.id.start_picker);
+                final DatePicker end_datePicker = (DatePicker) findViewById(R.id.end_picker);
+                //日期格式
+                String start_date_str="";
+                String end_date_str="";
+                start_date_str= String.valueOf(datePicker.getYear()+"年"+(datePicker.getMonth()+1)+"月"+datePicker.getDayOfMonth()+"日");
+                end_date_str= String.valueOf(end_datePicker.getYear()+"年"+(end_datePicker.getMonth()+1)+"月"+end_datePicker.getDayOfMonth()+"日");
+                start_date.setText(start_date_str);
+                end_date.setText(end_date_str);
+            }
+        });
+    }
+}
